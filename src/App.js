b7881be9d5063e84d446eeefed5dd4a692ea30ea
riccrusso2 <62103268+riccrusso2@users.noreply.github.com>
2025-09-30 08:37:10 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -204,7 +204,7 @@ export default function PortfolioDashboard() {
     ];
   });
 
-  const MONTHLY_BUDGET = 2500; // € da investire ogni mese
+  const MONTHLY_BUDGET = 5500; // € da investire ogni mese
   const totalCash = 10000; // esempio, la liquidità totale
 
   const [history, setHistory] = useState(() => {

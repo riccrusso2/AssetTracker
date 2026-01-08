66afc98b428499556d598636a543e1a64ae44543
riccrusso2 <62103268+riccrusso2@users.noreply.github.com>
2026-01-08 10:37:27 +0100

    Update MONTHLY_BUDGET from 500 to 800

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ const STORAGE_KEYS = {
   PRIVATE_EQUITY: "pf.privateequity.v1",
 };
 
-const MONTHLY_BUDGET = 500;
+const MONTHLY_BUDGET = 800;
 const TOTAL_CASH = 10500;
 const AUTO_REFRESH_INTERVAL = 900000; // 15 minutes
 
